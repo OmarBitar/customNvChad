8ebbab33e6c93775a0d9a227caa278deaf648122
omar bitar <o.bitar97@gmail.com>
2024-11-24 21:18:52 +0300

    bug fix -- added comments for vue and tsx/jsx

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,18 +1,19 @@
--- load defaults i.e lua_lsp
+-- lspconfig.lue
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
+-- List of LSP servers
 local servers = { 
   "html",
   "cssls",
-  "ts_ls",
+  "ts_ls",  -- Use ts_ls instead of tsserver
   "volar"
 }
+
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
+-- Setup LSP servers with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
@@ -21,33 +22,21 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
-
--- vue language server
+-- Vue language server specific configuration
 local mason_registry = require('mason-registry')
 local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
-local lspconfig = require('lspconfig')
 
-require'lspconfig'.ts_ls.setup{
+lspconfig.ts_ls.setup {
   init_options = {
     plugins = {
       {
         name = '@vue/typescript-plugin',
         location = vue_language_server_path,
-        languages = {"javascript", "typescript", "vue"},
+        languages = { 'javascript', 'typescript', 'vue' },
       },
     },
   },
-  filetypes = {
-    "javascript",
-    "typescript",
-    "vue",
-  },
+  filetypes = { 'javascript', 'typescript', 'vue' },
 }
 
 lspconfig.volar.setup {
